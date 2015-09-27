fb8c0642c5cb1d0876a4a5a85639edc6427da87b
eledero <delarosaelias1@gmail.com>
2015-09-27 11:01:35 -0500

    Final commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,3 @@
-### Download files from the web###
-
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-
-download.file(url, "datafile.zip")
-
 
 ###Read headers, training set and test dataset 
 
